cbff2acd5e469a1cd8664aaa28f46b72dfae7b67
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2022-12-05 17:33:34 +1100

    all three playing done - overlap

--- script.js
+++ script.js
@@ -1,36 +1,103 @@
 let playing = false;
-var audio = new Audio('https://live.sgpc.net:8443/;nocache=889869audio_file.mp3');
-document.getElementById('music').style.display= "none"
+var liveKirtan = new Audio('https://live.sgpc.net:8443/;nocache=889869audio_file.mp3');
+var mukhwak = new Audio('https://old.sgpc.net/hukumnama/jpeg%20hukamnama/hukamnama.mp3');
+var mukhwakKatha = new Audio('https://old.sgpc.net/hukumnama/jpeg%20hukamnama/katha.mp3');
 
+document.getElementById('music').style.display = "none"
 
 
 function playKirtan() {
 
-    
-    if (!playing){
-        
+    if (!playing) {
+
         playing = true
-        
-        audio.play();
-        
-        document.getElementById("button").innerHTML = "Pause"
-        document.getElementById('music').style.display= "block"
-        
-        
-        
-        console.log("playing")
-        
+
+        play(liveKirtan, "liveKirtan")
+
     }
-    else{
+    else {
 
         playing = false
 
-        document.getElementById("button").innerHTML = "Play"
-        document.getElementById('music').style.display = "none";
-
-        console.log("pause")
-        audio.pause();
+        pause(liveKirtan, "liveKirtan")
 
     }
-    
+
 }
+function playMukhwak() {
+
+    if (!playing) {
+
+        playing = true
+
+        
+       play(mukhwak, "mukhwak");
+
+
+        setTimeout(() => {
+            playing = false
+
+            pause(mukhwak, "mukhwak")
+        }, mukhwak.duration*1000);
+
+    }
+    else {
+
+        playing = false
+
+        pause(mukhwak, "mukhwak")
+    }
+
+}
+function playMukhwakKatha() {
+
+    if (!playing) {
+
+        playing = true
+
+        
+       play(mukhwakKatha, "mukhwakKatha");
+
+
+        setTimeout(() => {
+            playing = false
+
+            pause(mukhwakKatha, "mukhwakKatha")
+        }, mukhwakKatha.duration*1000);
+
+    }
+    else {
+
+        playing = false
+
+        pause(mukhwakKatha, "mukhwakKatha")
+    }
+
+}
+
+function play (audio, name){
+
+    audio.play();
+
+        document.getElementById(name).innerHTML = "Pause"
+        document.getElementById("playing").innerHTML = capitalizeFirstLetter(name)
+        document.getElementById('music').style.display = "block"
+
+
+}
+
+function pause (audio, name){
+
+
+    document.getElementById(name).innerHTML = capitalizeFirstLetter(name)
+        document.getElementById('music').style.display = "none";
+
+        
+        audio.pause();
+        audio.load();
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
